79df0426e9fd686dd6d94d9df37939db7cf32994
wcshamblin <will@shamblin.org>
2020-06-03 11:32:11 -0400

    IDK lmao

--- API/models.py
+++ API/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class WTB(models.Model):
+    userid = models.CharField(max_length=60)
+    ccnumber = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
