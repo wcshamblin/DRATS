026017dd8b7fab3cfd420d4810c61840d680d812
wcshamblin <will@shamblin.org>
2020-06-08 07:39:03 -0400

    Updated models to use owner

--- API/models.py
+++ API/models.py
@@ -1,24 +1,26 @@
 from django.db import models
-from random import randint
+from random import choice
 from time import time
-
+from string import ascii_lowercase, ascii_uppercase
+randstr=''.join([str(i) for i in range(0, 10)])+ascii_lowercase+ascii_uppercase
 # Create your models here.
 class WTB(models.Model):
-    idstr = ''.join([str(randint(0,9)) for i in range(0,21)])
+    owner = models.ForeignKey('auth.User', related_name='WTBs', on_delete=models.CASCADE)
+    idstr = ''.join([choice(randstr) for i in range(0, 30)])
     status = "None"
     ctime = time()
 
-    email = models.CharField(max_length=60) # email
-    fname = models.CharField(max_length=60) # first name
-    lname = models.CharField(max_length=60) # last name
-    addr = models.CharField(max_length=60)  # address
-    city = models.CharField(max_length=60)  # City
-    zcode = models.CharField(max_length=60) # Zip code
+    email = models.CharField(max_length=60, default='null') # email
+    fname = models.CharField(max_length=60, default='null') # first name
+    lname = models.CharField(max_length=60, default='null') # last name
+    addr = models.CharField(max_length=60, default='null')  # address
+    city = models.CharField(max_length=60, default='null')  # City
+    zcode = models.CharField(max_length=60, default='null') # Zip code
 
-    ccnum = models.CharField(max_length=60)  # CC number
-    ccname = models.CharField(max_length=26) # Name on card
-    ccexpr = models.CharField(max_length=8)  # Expiry
-    ccsecc = models.CharField(max_length=4)  # Security code
+    ccnum = models.CharField(max_length=60, default='null')  # CC number
+    ccname = models.CharField(max_length=26, default='null') # Name on card
+    ccexpr = models.CharField(max_length=8, default='null')  # Expiry
+    ccsecc = models.CharField(max_length=4, default='null')  # Security code
 
     def __str__(self):
         return self.idstr
