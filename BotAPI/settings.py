bd401fac0412ffd995e23765e1d8d06c040b22b7
wcshamblin <will@shamblin.org>
2020-06-24 09:16:32 -0400

    Read key from /var/

--- BotAPI/settings.py
+++ BotAPI/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rn*8y@mj*3cwf3qyb!cckdc_4wh65_=^wba54=611o=k9b7#75'
+SECRET_KEY = ''
+with open('/var/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
