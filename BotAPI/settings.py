ce26cc469a03161078a0d5688db2c678951f87e2
wcshamblin <will@shamblin.org>
2020-06-24 09:02:09 -0400

    Debug = False

--- BotAPI/settings.py
+++ BotAPI/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rn*8y@mj*3cwf3qyb!cckdc_4wh65_=^wba54=611o=k9b7#75'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
